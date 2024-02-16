0ee5c74d16c087ef2eeeda011fccad328cc39c97
YosYas <yamayuu1549@gmail.com>
2024-02-16 14:11:31 +0900

    240216-1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 'use strict';
 {
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+// インラインブートストラップローダーのコールバック関数としてinitMapを登録
+// google.maps.__ib__ = initMap;
+window.initMap = initMap;
 
 
 
-
-
-
-  
 }
