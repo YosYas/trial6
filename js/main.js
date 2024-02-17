d36609a7d1dc112652c21df7de4607d2b8c4bb99
Yos Yas <yamayuu1549@gmail.com>
2024-02-17 23:32:10 +0900

    240217-last

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 {
-  // GoogleMap
+// GoogleMap
   let map;
   async function initMap() {
     const kirameki = { lat: 35.64373, lng: 139.79450 };
@@ -12,4 +12,37 @@
 }
   initMap();
 
+// バリデーション
+  const form = document.querySelector("#entryform");
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let valid = true;
+
+    // 各inputを検証
+    [...form.elements].forEach(input => {
+      if (input.name !== 'payment') {
+        const errorMessage = input.nextElementSibling;
+        if (errorMessage) {
+          if (!input.checkValidity()) {
+            valid = false;
+            input.classList.add('invalid');
+            errorMessage.textContent = input.validationMessage;
+          } else if (input.id === 'address' && !input.value.match(/^\d{7}$/)) {
+            errorMessage.textContent = "郵便番号は7文字の数字で入力してください。";
+          } else if (input.id === 'city' && !input.value.match(/^[^\x20-\x7e]*$/)) {
+            errorMessage.textContent = "住所は全角文字で入力してください。";
+          } else if (input.id === 'tel' && !input.value.match(/^0\d{10,11}$/)) {
+            errorMessage.textContent = "半角かつ10桁or11桁の数字で入力してください。";
+          } else if (
+            input.id === 'password' && !input.value.match(/^([a-zA-Z0-9]{4,})$/)) {
+            errorMessage.textContent = "4文字以上の英数字を入力してください。";
+          } else {
+            input.classList.remove("invalid");
+            input.setCustomValidity("");
+            errorMessage.textContent = "";
+          }
+        }
+      }
+    });
+  });
 }
